fefead0cd939ea54d93aef07a5b3e275ab8ac2b7
devonyou <onyou.code@gmail.com>
2025-07-15 16:42:37 +0900

    test: jest 설정 파일 수정

--- apps/user/jest.config.ts
+++ apps/user/jest.config.ts
@@ -2,14 +2,29 @@ import type { Config } from 'jest';
 
 const config: Config = {
     displayName: 'user',
+    rootDir: '.',
     testEnvironment: 'node',
     moduleFileExtensions: ['ts', 'js', 'json'],
     transform: {
         '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
     },
-    testMatch: ['**/*.spec.ts'],
-    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.module.ts', '!src/main.ts'],
+    testMatch: ['<rootDir>/**/*.spec.ts'],
+    collectCoverageFrom: [
+        '<rootDir>/**/*.{ts,js}',
+        '!<rootDir>/**/*main.ts',
+        '!<rootDir>/**/*module.ts',
+        '!<rootDir>/**/*entity.ts',
+        '!<rootDir>/**/*dto.ts',
+        '!<rootDir>/**/*config.ts',
+        '!<rootDir>/**/*schema.ts',
+        '!<rootDir>/**/*strategy.ts',
+        '!<rootDir>/**/*spec.ts',
+    ],
     coverageDirectory: '../../coverage/apps/user',
+    moduleNameMapper: {
+        // '@/(.*)': '<rootDir>/src/$1',
+        '^@app/common(.*)$': '<rootDir>/../../libs/common/src$1',
+    },
 };
 
 export default config;
--- apps/user/src/modules/user/user.service.spec.ts
+++ apps/user/src/modules/user/user.service.spec.ts
@@ -1,5 +1,89 @@
-describe('Example test', () => {
-    it('should work', () => {
-        expect(true).toBe(true);
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
+import { DataSource } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { RedisService, UserMicroService } from '@app/common';
+import { UserEntity } from '../../entities/user.entity';
+
+const mockUserRepository = {
+    findOne: jest.fn(),
+    findOneBy: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    update: jest.fn(),
+};
+
+const mockJwtService = {
+    sign: jest.fn(),
+    verify: jest.fn(),
+};
+
+const mockConfigService = {
+    getOrThrow: jest.fn(),
+};
+
+const mockRedisService = {
+    set: jest.fn(),
+    get: jest.fn(),
+    ttl: jest.fn(),
+    del: jest.fn(),
+};
+
+const mockDataSource = {
+    getRepository: jest
+        .fn(entity => {
+            if (entity === UserEntity) return mockUserRepository;
+        })
+        .mockReturnValue(mockUserRepository),
+};
+
+describe('UserService', () => {
+    let userService: UserService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                UserService,
+                { provide: DataSource, useValue: mockDataSource },
+                { provide: JwtService, useValue: mockJwtService },
+                { provide: ConfigService, useValue: mockConfigService },
+                { provide: RedisService, useValue: mockRedisService },
+            ],
+        }).compile();
+
+        userService = module.get<UserService>(UserService);
+    });
+
+    it('should create new user if not found', async () => {
+        const userInfo = {
+            provider: 'google',
+            providerId: '1234',
+            email: 'test@example.com',
+            name: 'onyou.code',
+            avatarUrl: 'https://example.com/avatar.png',
+            emailVerified: true,
+        };
+
+        // findOne returns null → 사용자 없음
+        mockUserRepository.findOne.mockResolvedValue(null);
+
+        // create mock
+        const createdUser = { ...userInfo, role: UserMicroService.UserRole.USER };
+        mockUserRepository.create.mockReturnValue(createdUser);
+        mockUserRepository.save.mockResolvedValue({ id: 'user-id', ...createdUser });
+
+        const result = await userService.findOrCreateUserByGoogle(userInfo);
+
+        expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+            where: { provider: 'google', providerId: '1234' },
+        });
+        expect(mockUserRepository.create).toHaveBeenCalledWith({
+            ...userInfo,
+            role: UserMicroService.UserRole.USER,
+        });
+        expect(mockUserRepository.save).toHaveBeenCalledWith(createdUser);
+
+        expect(result).toEqual({ id: 'user-id', ...createdUser });
     });
 });
